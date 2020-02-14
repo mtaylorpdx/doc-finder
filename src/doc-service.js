515fbdb9bb674137157d5cbeddb7da9bf78bbac0
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-14 11:05:45 -0800

    get form info and pass into api function

--- src/doc-service.js
+++ src/doc-service.js
@@ -1,8 +1,10 @@
 export class DocService {
-  
+
   async getDocInfoByIssue() {
     try {
-      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?query=flu&location=OR&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      console.log(issue);
+      console.log(location);
+      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=${location}&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       if (response.status != 200 || response.ok != true) {
         return false;
       } else {
--- src/main.js
+++ src/main.js
@@ -9,12 +9,26 @@ import { DocService } from './doc-service.js';
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+
+    const issue = $('#issue').val();
+    const location = $('#location').val();
+    console.log(issue + " " + location);
+    $('#issue').val("");
+    $('#location').val("");
+
+    (async () => {
+      let docService = new DocService();
+      const response = await docService.getDocInfoByIssue();
+      getElements(response);
+    })();
   });
 
-  (async () => {
-    let docService = new DocService();
-    const response = await docService.getDocInfoByIssue();
-    console.log(response);
-  })();
+  const getElements = function(response) {
+    if (response === false) {
+      console.log("Error");
+    } else {
+      console.log(response);
+    }
+  }
 
 });
