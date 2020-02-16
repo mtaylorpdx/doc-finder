49f65f58aa25d963213a167ed1c9874f7d04c262
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-16 09:36:44 -0800

    check for website url and replace undefined listings

--- src/main.js
+++ src/main.js
@@ -37,12 +37,20 @@ $(document).ready(function() {
       return availability;
     };
 
+    let websiteCheck = function(website) {
+      if (website === undefined) {
+        website = "Website URL not available.";
+      }
+      return website;
+    };
+
     const displayInfo = function(doctors) {
       doctors.data.forEach(function(doctor) {
         let availability = availabilityCheck(doctor.practices[0].accepts_new_patients);
-        console.log (availability);
+        let website = websiteCheck(doctor.practices[0].website);
+
         $("#outputResults").append(`<p><strong>Name:</strong> ${doctor.profile.first_name} ${doctor.profile.last_name}<br><p><strong>Address:</strong> ${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state}, ${doctor.practices[0].visit_address.zip}
-        <br><p><strong>Phone: </strong>${doctor.practices[0].phones[0].number} (${doctor.practices[0].phones[0].type})</p><p><strong>Website:</strong> ${doctor.practices[0].website}</p><p><strong>Availability:</strong> ${availability}</p><hr>`);
+        <br><p><strong>Phone: </strong>${doctor.practices[0].phones[0].number} (${doctor.practices[0].phones[0].type})</p><p><strong>Website:</strong> ${website}</p><p><strong>Availability:</strong> ${availability}</p><hr>`);
       });
     };
   });
