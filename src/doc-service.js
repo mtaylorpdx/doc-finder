5c94ed9f340ddc6a4e807a857b6841a55ccab159
mtaylorpdx <taylor.matt@protonmail.com>
2020-02-16 11:39:21 -0800

    refactor

--- src/doc-service.js
+++ src/doc-service.js
@@ -1,20 +1,3 @@
-// export class DocService {
-
-//   async getDocInfoByIssue(issue, docName) {
-//     try {
-//       let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=37.773%2C-122.413&name=${docName}&query=${issue}&skip=0&limit=20&user_key=${process.env.API_KEY}`);
-//       if (response.status != 200 || response.ok != true) {
-//         return false;
-//       } else {
-//         let jsonifiedResponse = await response.json();
-//         return jsonifiedResponse;
-//       }
-//     } catch(error) {
-//       return false;
-//     }
-//   }
-// }
-
 export class DocService {
   async getDocInfoByIssue(issue, docName) {
     try {
--- src/main.js
+++ src/main.js
@@ -20,18 +20,6 @@ $(document).ready(function() {
       getElements(response);
     })();
 
-    // const getElements = function(response) {
-    //   console.log(response);
-    //   if (response === true) {
-    //     displayInfo(response);
-    //   } else if (response.data.length === 0) {
-    //     $("#outputResults").append("No results were found matching your search criteria.");
-    //   } else {
-    //     $("#outputResults").append("There was an error with your request. Please double-check your entries.");
-    //   }
-    // };
-
-
     const getElements = function(response) {
       if (response.data.length === 0) {
         $("#outputResults").append("No results were found matching your search criteria.");
@@ -62,7 +50,6 @@ $(document).ready(function() {
       doctors.data.forEach(function(doctor) {
         let availability = availabilityCheck(doctor.practices[0].accepts_new_patients);
         let website = websiteCheck(doctor.practices[0].website);
-
         $("#outputResults").append(`<p><strong>Name:</strong> ${doctor.profile.first_name} ${doctor.profile.last_name}<br><p><strong>Address:</strong> ${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state}, ${doctor.practices[0].visit_address.zip}
         <br><p><strong>Phone: </strong>${doctor.practices[0].phones[0].number} (${doctor.practices[0].phones[0].type})</p><p><strong>Website:</strong> ${website}</p><p><strong>Availability:</strong> ${availability}</p><hr>`);
       });
